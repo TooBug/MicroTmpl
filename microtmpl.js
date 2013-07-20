dda43dcd21f78725850d84e74eefa0b29808502f
TooBug <i@toobug.net>
2013-07-20 10:11:45 +0800

    兼容Node.js

--- microtmpl.js
+++ microtmpl.js
@@ -1,16 +1,28 @@
-function MicroTmpl(tmpl,data){
-	var itemdata;
-	function strReplace(match,itemName){
-		return itemdata[itemName] || '';
-	}
-	if(typeof data.length === 'undefined'){
-		data = [data];
-	}
+~function(){
 	
-	var ret = '';
-	data.forEach(function(dataItem){
-		itemdata = dataItem;
-		ret += tmpl.replace(/\{%(\w+)%\}/g,strReplace);
-	});
-	return ret;
-}
+	var microTmpl = function(tmpl,data){
+
+		var itemdata;
+		function strReplace(match,itemName){
+			return itemdata[itemName] || '';
+		}
+		if(typeof data.length === 'undefined'){
+			data = [data];
+		}
+		
+		var ret = '';
+		data.forEach(function(dataItem){
+			itemdata = dataItem;
+			ret += tmpl.replace(/\{%(\w+)%\}/g,strReplace);
+		});
+		return ret;
+
+	};
+
+	if(typeof window !== 'undefined'){
+		window.microTmpl = window.MicroTmpl = microTmpl;
+	}else{
+		module.exports = microTmpl;
+	}
+
+}();
